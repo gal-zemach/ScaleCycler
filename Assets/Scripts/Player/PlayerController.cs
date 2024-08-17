c482e59a8b3252caaa31560e350367c77f98fff4
Gal Zemach <>
2024-08-16 22:13:26 -0700

    removed comments + name changes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,50 +3,36 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public PlayerMover environmentMover;
+    public PlayerMover playerMover;
 
     public Transform groundCheck;
     public LayerMask groundLayer;
 
     [Space]
     [Header("Controls")]
-    // public KeyCode ForwardsKey = KeyCode.D;
-    // public KeyCode BackwardsKey = KeyCode.A;
     public KeyCode JumpKey = KeyCode.W;
 
     private bool isGrounded;
 
+    private Vector2 mouseDragStartPos;
+    private Vector2 mouseDragEndPos;
+
     void Update()
     {
-        if (environmentMover == null)
-        {
-            return;
-        }
-        
         CheckGrounded();
 
         // Handle horizontal movement based on input
         float moveInput = Input.GetAxisRaw("Horizontal"); // Returns -1, 0, or 1 for left, no input, or right
         if (moveInput != 0)
         {
-            environmentMover.MoveHorizontal(moveInput);
+            playerMover.MoveHorizontal(moveInput);
         }
 
         // Trigger environment jump when the spacebar is pressed and the player is grounded
         if (Input.GetKeyDown(JumpKey) && isGrounded)
         {
-            environmentMover.Jump();
+            playerMover.Jump();
         }
-
-        // // Example: Change the shape of the wheel
-        // if (Input.GetKeyDown(KeyCode.Alpha1))
-        // {
-        //     ChangeWheelShape(1);
-        // }
-        // else if (Input.GetKeyDown(KeyCode.Alpha2))
-        // {
-        //     ChangeWheelShape(2);
-        // }
     }
 
     void CheckGrounded()
@@ -54,10 +40,4 @@ public class PlayerController : MonoBehaviour
         // Check if the player is touching the ground
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
     }
-
-    // void ChangeWheelShape(int shapeIndex)
-    // {
-    //     // Implement your logic to change the wheel's shape here
-    //     Debug.Log("Wheel shape changed to: " + shapeIndex);
-    // }
 }
