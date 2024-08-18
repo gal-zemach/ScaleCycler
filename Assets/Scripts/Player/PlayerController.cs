ca5aecec5eae6266ad80e95246bb285f95651326
Gal Zemach <>
2024-08-17 17:04:05 -0700

    somewhat working wheel

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,12 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public PlayerMover playerMover;
-
+    [Header("Components")]
+    public Rigidbody rigidBody;
+    public WheelCollider wheelCollider;
+    
+    [Space]
+    public Transform wheelSpriteTransform;
     public Transform groundCheck;
     public LayerMask groundLayer;
+
+    [Space]
+    [Header("Parameters")]
+    public float motorTorque = 100f;
+    public float brakeTorque = 100f;
+    public float maxSpeed = 5f;
 
     [Space]
     [Header("Controls")]
@@ -14,30 +25,66 @@ public class PlayerController : MonoBehaviour
 
     private bool isGrounded;
 
-    private Vector2 mouseDragStartPos;
-    private Vector2 mouseDragEndPos;
+    private Vector3 position;
+    private Quaternion rotation;
 
     void Update()
     {
+        // Get the Wheel collider's world pose values and
+        // use them to set the wheel model's position and rotation
+        wheelCollider.GetWorldPose(out position, out rotation);
+        wheelSpriteTransform.position = position;
+        wheelSpriteTransform.rotation = rotation;
+    }
+
+    void FixedUpdate()
+    {
         CheckGrounded();
 
-        // Handle horizontal movement based on input
+        // Calculate current speed in relation to the forward direction of the car
+        // (this returns a negative number when traveling backwards)
+        float forwardSpeed = Vector3.Dot(Vector3.right, rigidBody.velocity);
+
+        // Calculate how close the car is to top speed
+        // as a number from zero to one
+        float speedFactor = Mathf.InverseLerp(0, maxSpeed, forwardSpeed);
+
+        // Use that to calculate how much torque is available 
+        // (zero torque at top speed)
+        float currentMotorTorque = Mathf.Lerp(motorTorque, 0, speedFactor);
+
         float moveInput = Input.GetAxisRaw("Horizontal"); // Returns -1, 0, or 1 for left, no input, or right
         if (moveInput != 0)
         {
-            playerMover.MoveHorizontal(moveInput);
+            // Check whether the user input is in the same direction 
+            // as the car's velocity
+            bool isAccelerating = Mathf.Sign(moveInput) == Mathf.Sign(forwardSpeed) || Mathf.Abs(forwardSpeed) < 1f;
+            if (isAccelerating)
+            {
+                wheelCollider.motorTorque = Mathf.Sign(moveInput) * currentMotorTorque;
+                wheelCollider.brakeTorque = 0f;
+            }
+            else
+            {
+                // If the user is trying to go in the opposite direction
+                // apply brakes to all wheels
+                wheelCollider.brakeTorque = brakeTorque;
+                wheelCollider.motorTorque = 0;
+            }
+            
+            // Debug.Log($"Inpt: ({moveInput}); FwdSp: ({forwardSpeed}); SpFctr: ({speedFactor}); CurrMtrTrq:({currentMotorTorque})");
         }
 
         // Trigger environment jump when the spacebar is pressed and the player is grounded
         if (Input.GetKeyDown(JumpKey) && isGrounded)
         {
-            playerMover.Jump();
+            // playerMover.Jump();
         }
     }
 
     void CheckGrounded()
     {
         // Check if the player is touching the ground
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        isGrounded = Physics.OverlapSphere(groundCheck.position, 0.1f, groundLayer)?.Length > 0;
     }
 }
